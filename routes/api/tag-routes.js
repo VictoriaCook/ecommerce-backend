7bde3cdd7c881bd66d95bf2db570dc60bab2fe04
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-02-21 14:18:08 +1100

    added tag get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,14 +3,35 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    let tags = await Tag.findAll({
+      include: [Product],
+    });
+    res.json(tags);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    let singleTag = await Tag.findOne({
+      where: {
+        id: req.params.id,
+      },
+      include: [Product],
+    });
+    res.json(singleTag)
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
