a60268d166a8fe10d101fcd4dd21cb6d70edccc9
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-02-21 13:52:29 +1100

    added new category post request route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,8 +36,15 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    let newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  }
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
